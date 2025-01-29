977f785a4f2e0be615f87b8455a149c76d54121b
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-01-29 18:17:28 +0800

    add controllers for Category, Courses, GeneralInformation, Lecturers, and Virtual resources

---
+++ app/Http/Controllers/VirtualController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Virtual;
+use Illuminate\Http\Request;
+
+class VirtualController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Virtual $virtual)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Virtual $virtual)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Virtual $virtual)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Virtual $virtual)
+    {
+        //
+    }
+}
