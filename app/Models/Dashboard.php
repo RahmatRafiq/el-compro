fa67f515b5b59a3c774e637e34b6a45c7f2d75e3
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-01-29 17:31:56 +0800

    refactor Dashboard model to consolidate dashboard data retrieval methods

--- app/Models/Dashboard.php
+++ app/Models/Dashboard.php
@@ -14,59 +14,11 @@ class Dashboard extends Model
 {
     use HasFactory;
 
-    public static function getPesertaCount($user)
+    public function getDashboardData()
     {
-        return Peserta::whereHas('registrationPlacement.lowongan.mitra', function ($query) use ($user) {
-            $query->where('user_id', $user->id);
-        })->count();
-    }
-
-    public static function getLowonganData($user)
-    {
-        return Lowongan::whereHas('mitra', function ($query) use ($user) {
-            $query->where('user_id', $user->id);
-        })->withCount('registrations')->get();
-    }
-
-    public static function getCounts()
-    {
-        return [
-            'peserta' => Peserta::count(),
-            'dosen' => DosenPembimbingLapangan::count(),
-            'mitra' => MitraProfile::count(),
-            'lowongan' => Lowongan::count(),
-            'laporanHarian' => LaporanHarian::count(),
-            'laporanMingguan' => LaporanMingguan::count(),
-            'laporanLengkap' => LaporanLengkap::count(),
+        $data = [
+            'total_users' => User::count(),
         ];
-    }
-
-    public static function getLaporanHarianStatusCounts()
-    {
-        return LaporanHarian::selectRaw('status, count(*) as count')
-            ->groupBy('status')
-            ->get()
-            ->pluck('count', 'status')
-            ->toArray();
-    }
-
-    public static function getLaporanMingguanStatusCounts()
-    {
-        return LaporanMingguan::selectRaw('status, count(*) as count')
-            ->groupBy('status')
-            ->get()
-            ->pluck('count', 'status')
-            ->toArray();
-    }
-
-    public static function getStaffDashboardData()
-    {
-        // $jumlahPesertaAktif = Peserta::where('status', 'aktif')->count();
-        $laporanHarian = LaporanHarian::count();
-        $laporanMingguan = LaporanMingguan::count();
-        $lowonganTersedia = Lowongan::where('is_open', true)->count();
-        $pesertaTerbaru = Peserta::orderBy('created_at', 'desc')->take(5)->get();
-
-        return compact( 'laporanHarian', 'laporanMingguan', 'lowonganTersedia', 'pesertaTerbaru');
+        return $data;
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
-            DosenPembimbingSeeder::class,
-            LaporanLengkapSeeder::class,
             UserSeeder::class,
         ]);
     }
 }
+
