59b4fbc8f384b7b25d7ab1b3a9139430adfef011
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-06 18:23:17 +0800

    feat: add React and Inertia.js support with TypeScript configuration

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/dropzoner.js'
+                'resources/js/dropzoner.js',
+                'resources/js/react.tsx',
             ],
             refresh: true,
         }),
