f3edf92d294fd4579bedf5e68ed0c2c8b7bde1de
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-06 21:09:53 +0800

    feat: add layout components including Header, Footer, Layout, and ThemeController

---
+++ resources/js/Layouts/Layout.tsx
@@ -0,0 +1,22 @@
+// resources/js/Layouts/Layout.tsx
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import ThemeController from './ThemeController';
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <Header />
+            <main className="px-4 py-6">{children}</main>
+            <Footer />
+            <ThemeController />
+        </div>
+    );
+};
+
+export default Layout;
