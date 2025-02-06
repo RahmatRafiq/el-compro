a1483bb3637c95369c2813874fe8ee8d1a3a9431
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-06 18:23:27 +0800

    feat: add Inertia.js middleware for handling requests and shared data

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            // 'auth.optional' => \App\Http\Middleware\AuthOptional::class,
+            'inertia' => HandleInertiaRequests::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
