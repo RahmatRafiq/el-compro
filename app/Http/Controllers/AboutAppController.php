3794d71dc8431202f7c7ae0f85e62d1980850017
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-04-17 02:14:23 +0800

    feat: enhance AboutApp model and controller to support media uploads for 'struktur-organisasi'

--- app/Http/Controllers/AboutAppController.php
+++ app/Http/Controllers/AboutAppController.php
@@ -1,7 +1,7 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Helpers\MediaLibrary;
 use App\Models\AboutApp;
 use Illuminate\Http\Request;
 
@@ -12,26 +12,42 @@ class AboutAppController extends Controller
      */
     public function index()
     {
-        // Mengambil semua entri AboutApp
-        $aboutApp = AboutApp::first();
-
-        return view('admin.about-app.index', compact('aboutApp'));
+        $aboutApp           = AboutApp::first();
+        $strukturOrganisasi = $aboutApp->getMedia('struktur-organisasi')->first();
+        return view('admin.about-app.index', compact('aboutApp', 'strukturOrganisasi'));
     }
 
-    /**
-     * Update the profile in storage.
-     */
+    public function upload(Request $request)
+    {
+        $request->validate([
+            'struktur-organisasi.*' => 'required|file|max:2048|mimes:jpeg,jpg,png',
+            'id'                    => 'required|integer',
+        ]);
+
+        AboutApp::find($request->id);
+
+        return response()->json(['message' => 'Image uploaded'], 200);
+    }
+
     public function update(Request $request, $id)
     {
         $request->validate([
-            'title' => 'required|string|max:255',
-            'description' => 'required|string',
-            'contact_email' => 'nullable|string|email|max:255',
-            'contact_phone' => 'nullable|string|max:20',
+            'title'           => 'required|string|max:255',
+            'description'     => 'required|string',
+            'contact_email'   => 'nullable|string|email|max:255',
+            'contact_phone'   => 'nullable|string|max:20',
             'contact_address' => 'nullable|string|max:255',
         ]);
-
         $aboutApp = AboutApp::find($id);
+
+        if ($request->has('struktur-organisasi')) {
+            MediaLibrary::put(
+                $aboutApp,
+                'struktur-organisasi',
+                $request,
+                'struktur-organisasi'
+            );
+        }
 
         if ($aboutApp) {
             $aboutApp->update($request->all());
@@ -45,16 +61,19 @@ class AboutAppController extends Controller
             ->with('success', $message);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    public function deleteFile(Request $request)
+    {
+        $aboutApp = AboutApp::find($request->id);
+        $aboutApp->clearMediaCollection('struktur-organisasi');
+        return response()->json(['message' => 'Image deleted'], 200);
+    }
     public function store(Request $request)
     {
         $request->validate([
-            'title' => 'required|string|max:255',
-            'description' => 'required|string',
-            'contact_email' => 'nullable|string|email|max:255',
-            'contact_phone' => 'nullable|string|max:20',
+            'title'           => 'required|string|max:255',
+            'description'     => 'required|string',
+            'contact_email'   => 'nullable|string|email|max:255',
+            'contact_phone'   => 'nullable|string|max:20',
             'contact_address' => 'nullable|string|max:255',
         ]);
 
--- app/Models/AboutApp.php
+++ app/Models/AboutApp.php
@@ -1,19 +1,24 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class AboutApp extends Model
+class AboutApp extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $table = 'about_app';
 
     protected $fillable = [
         'title',
         'description',
+        'greeting',
+        'vision_mission',
+        'history',
+        'vision',
         'contact_email',
         'contact_phone',
         'contact_address',
